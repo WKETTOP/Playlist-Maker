465b05178ea6c6c1e4a7012eceb8eaf9d8c473f4
Daniil <dan-07102000@mail.ru>
2024-12-09 15:21:33 +0300

    Реализовал задачи спринта №9: 1) Стили и темы а. Создал общий стиль для атрибутов на «Главном экране» и в «Настройках». б. Вставил новые шрифты в экраны. в. Убрал тени у кнопок. г. Адаптировал интерфейс под «Ночной режим». 2) Context и Intent а. Перенесл все строки с экранов «Главный экран» и «Настройки» в strings.xml. б. Реализовал кнопки «Поделиться приложением», «Написать разработчикам», «Пользовательское соглашение». 3) EditText и TextWatcher а. Реализовал экран «Поиск» без выполнения поискового запроса и отображения списка результатов. б. Реализовал экран, который содержит заголовок, кнопку «Назад» и поле ввода поискового запроса с кнопкой «Очистить». 4) Хранение данных. Начало а. Реализовал сохранение текста поискового запроса в жизненном цикле SearchActivity.

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
@@ -20,10 +21,43 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         val backButton = findViewById<ImageView>(R.id.back_button)
+        val shareButton = findViewById<ImageView>(R.id.share_button)
+        val supportButton = findViewById<ImageView>(R.id.support_button)
+        val userAgreementButton = findViewById<ImageView>(R.id.user_agreement_button)
 
         backButton.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+        }
+
+        shareButton.setOnClickListener {
+            val urlAndroidDev = getString(R.string.url_android_dev)
+
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, urlAndroidDev)
+                type = "text/plain"
+            }
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_app_line)))
+        }
+
+        supportButton.setOnClickListener {
+            val email = getString(R.string.support_email)
+            val subject = getString(R.string.support_email_subject)
+            val body = getString(R.string.support_email_body)
+
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, body)
+            }
+            startActivity(supportIntent)
+        }
+
+        userAgreementButton.setOnClickListener {
+            val urlUserAgreement = getString(R.string.url_user_agreement)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(urlUserAgreement))
+            startActivity(browserIntent)
         }
     }
 }
